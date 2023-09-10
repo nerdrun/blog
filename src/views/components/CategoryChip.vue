90a5fd6f4f96262a0fff0271a0859c214df7c1d9
kai <nerdrun.kai@gmail.com>
2023-09-10 17:40:56 +0200

    added components folder

---
+++ src/views/components/CategoryChip.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { computed, PropType } from 'vue';
+import { Category } from '../../enums/category.enum';
+
+defineProps({
+  category: { required: true, type: String as PropType<Category>, default: '' },
+});
+
+const color = computed(() => {
+  return '';
+});
+</script>
+<template>
+</template>
+<style scoped></style>
