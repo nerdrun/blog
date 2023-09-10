409db867ef855a8847cc5851c35721cc3dadbe9a
kai <nerdrun.kai@gmail.com>
2023-09-10 17:40:18 +0200

    added posts

---
+++ src/constants/posts.ts
@@ -0,0 +1,17 @@
+import { Category } from '../enums/category.enum';
+import { Platform } from '../enums/platform.enum';
+export default [
+  {
+    year: 2022,
+    contents: [
+      { 
+        category: Category.BLOG,
+        title: 'Docker setup on AWS with NestJS in 5 minutes',
+        platform: Platform.MEDIUM,
+        date: 'Dec 23, 2022',
+        image: new URL('../assets/posts/2022/docker_setup_on_aws_with_nestjs_in_5_minutes.webp', import.meta.url).href,
+        link: 'https://nerdrun.medium.com/docker-setup-on-aws-with-nestjsin-5-minutes-f5f2447d17b4',
+      }
+    ]
+  }
+]
