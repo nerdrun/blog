746960a45da1daaaa7daf63826db3ad5dc13507e
kai <nerdrun.kai@gmail.com>
2023-09-10 17:39:17 +0200

    added logo images

--- src/views/AppBar.vue
+++ src/views/AppBar.vue
@@ -1,10 +1,28 @@
 <script setup lang="ts">
+const links = [
+  { name: 'github', path: new URL('../assets/logos/github.svg', import.meta.url).href, url: 'https://github.com/nerdrun' },
+  { name: 'linkedin', path: new URL('../assets/logos/linkedin.svg', import.meta.url).href, url: 'https://www.linkedin.com/in/nerdrun/' },
+  { name: 'medium', path: new URL('../assets/logos/medium.svg', import.meta.url).href, url: 'https://nerdrun.medium.com' },
+];
 </script>
 <template>
-  <div class="flex flex-col justify-center items-center sticky top-0 bg-black py-4 border-b-1 border-b-white">
-    <div class="text-2xl">
-      NERDRUN
+  <section class="flex justify-center sticky top-0 bg-light-dark py-4 border-b border-b-white">
+    <div class="w-[1024px] flex justify-between items-center px-4">
+      <div class="text-lg">
+        Nerdrun
+      </div>
+      <div class="flex justify-center itmes-center">
+        <div 
+          v-for="link in links" 
+          :key="link.name" 
+          class="w-10 h-10 flex justify-center items-center"
+        >
+          <a :href="link.url" target="_blank" class="cursor-pointer">
+            <img class="w-6 h-6" :src="link.path" :alt="link.name" />
+          </a>
+        </div>
+      </div>
     </div>
-  </div>
+  </section>
 </template>
 <style scoped></style>
