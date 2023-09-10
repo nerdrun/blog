1af5eb841ddcf38a5b1521b4057e67de5c0779f3
kai <nerdrun.kai@gmail.com>
2023-09-10 17:38:55 +0200

    added posts for loop

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <script setup lang="ts">
 import AppBar from './views/AppBar.vue';
+import Preface from './views/Preface.vue';
+import posts from './constants/posts';
 </script>
 <template>
   <AppBar />
-  <div class="">
-    Hello World
+  <div class="flex justify-center p-4 pt-10">
+    <Preface />
+  </div>
+  <div class="flex justify-center p-4 pt-10">
+    <section class="w-[1024px]">
+      <div v-for="post in posts" :key="post.year">
+        <div>{{ post.year }}</div>
+        <div v-for="(content, index) in post.contents" :key="index">
+          <div>{{ content.title }}</div>
+          <div>{{ content.link }}</div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 <style scoped></style>
