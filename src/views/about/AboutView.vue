47d7ae578caa14a66c520c39fe13ec4c758150f7
kai <nerdrun.kai@gmail.com>
2023-09-13 12:06:06 +0200

    added contents for about

--- src/views/about/AboutView.vue
+++ src/views/about/AboutView.vue
@@ -1,7 +1,49 @@
 <script setup lang="ts">
 </script>
 <template>
-  <router-link to="/">To Home</router-link>
-  <div>Yo</div>
+  <div class="flex justify-center p-5 pt-10">
+    <section class="w-[1024px] pb-16">
+      <div class="mb-10">
+        <h2 class="text-xl mb-4 font-semibold">ME</h2>
+        <ul class="pl-6">
+          <li class="list-disc mb-2">I'm from Jeonju, The South Korea.</li>
+          <li class="list-disc mb-2">Born in 1988.</li>
+          <li class="list-disc mb-2">I currently live in Barcelona, Spain.</li>
+          <li class="list-disc mb-2">Started studying programming in 2015, working as a professional software engineer in 2016.</li>
+          <li class="list-disc mb-2">
+            <div class="mb-3">Have been solving algorithm with JS everyday since 2023</div>
+            <img src="https://www.codewars.com/users/nerdrun/badges/large" />
+          </li>
+          <li class="list-disc mb-2">Got out of my room in which I made 24/7 coding and actually work with people in 2023.</li>
+          <li class="list-disc mb-2">Worked with many startups or mid-size companies. And curruently working in a huge company.</li>
+        </ul>
+      </div>
+      <div class="mb-10 border-b border-b-diminished-grey pb-8">
+        <h2 class="text-xl mb-4 font-semibold">FUN FACT</h2>
+        <ul class="pl-6">
+          <li class="list-disc mb-2">I graduated unversity of a convervatory of music.</li>
+          <li class="list-disc mb-2">I was a professional bassist in bands for 8 years.</li>
+          <li class="list-disc mb-2">I was a b-boy so I used to clean the floor with my entire body.</li>
+          <li class="list-disc mb-2">I watch the same movie at least three times.</li>
+        </ul>
+      </div>
+      <div>
+        <ul class="pl-6">
+          <li class="list-disc mb-2">
+            <a class="underline text-cyan-400 font-semibold" href="https://github.com/nerdrun">Github</a>
+          </li>
+          <li class="list-disc mb-2">
+            <a class="underline text-cyan-400 font-semibold" href="https://www.instagram.com/nerdrun">Instagram</a>
+          </li>
+          <li class="list-disc mb-2">
+            <a class="underline text-cyan-400 font-semibold" href="https://www.linkedin.com/in/nerdrun">LinkedIn</a>
+          </li>
+          <li class="list-disc mb-2">
+            <a class="underline text-cyan-400 font-semibold" href="mailto:nerdrun.kai@gmail.com">Email</a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  </div>
 </template>
 <style scoped></style>
--- src/views/components/AppBar.vue
+++ src/views/components/AppBar.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
 const links = [
   { name: 'github', path: new URL('../../assets/logos/github.svg', import.meta.url).href, url: 'https://github.com/nerdrun' },
-  { name: 'linkedin', path: new URL('../../assets/logos/linkedin.svg', import.meta.url).href, url: 'https://www.linkedin.com/in/nerdrun/' },
+  { name: 'linkedin', path: new URL('../../assets/logos/linkedin.svg', import.meta.url).href, url: 'https://www.linkedin.com/in/nerdrun' },
   { name: 'medium', path: new URL('../../assets/logos/medium.svg', import.meta.url).href, url: 'https://nerdrun.medium.com' },
 ];
 </script>
 <template>
   <section class="flex justify-center sticky top-0 bg-light-dark py-4 border-b border-b-white">
     <div class="w-[1024px] flex justify-between items-center px-6 md:px-0">
-      <div class="text-lg">Nerdrun</div>
+      <div class="text-lg">
+        <router-link to="/">Nerdrun</router-link>
+      </div>
       <div class="flex justify-center itmes-center">
         <div 
           v-for="link in links" 
